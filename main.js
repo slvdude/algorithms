1811360f2b30ce8fed299d44676fb735bae350cb
Сергей Плотников <sergejplotnikov@Sergejs-MacBook-Air.local>
2022-10-22 13:54:33 +0400

    fixes

--- main.js
+++ main.js
@@ -1,101 +1,112 @@
+// function createMatrix(a, b) {
+//     let matrix = new Array(a)
 
-// function createMatrix(a, b) { 
-//     let matrix = new Array(a) 
-    
-//     for (let i = 0; i < matrix.length; i++) { 
-//         matrix[i] = new Array(b) 
-//     } 
-    
-//     matrix = fillMatrix(matrix) 
-    
-//     return matrix 
-// } 
-    
-// function fillMatrix(matrix) { 
-//     for (let i = 0; i < matrix.length; i++) { 
-//         for (let j = 0; j < matrix[i].length; j++) { 
+//     for (let i = 0; i < matrix.length; i++) {
+//         matrix[i] = new Array(b)
+//     }
+
+//     matrix = fillMatrix(matrix)
+
+//     return matrix
+// }
+
+// function fillMatrix(matrix) {
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[i].length; j++) {
 //             matrix[i][j] = Math.round(Math.random() * 100)
-//         } 
-//     } 
+//         }
+//     }
 
-//     return matrix 
-// } 
-    
-function zhardanoGaussMethod(m, n, k, s) { 
-    let matrix = [ [ -3, 5, -3 ], [ -1, -2, 2 ]]
-    let matrixCopy = [ [ -3, 5, -3 ], [ -1, -2, 2 ]]
-    drawMatrix(matrix)
+//     return matrix
+// }
 
-    console.log(`k: ${k}\ns: ${s}`)
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-    if (matrix[k][s] <= 0) {
-        console.log("Элемент является не разрешимым")
-        return 
+let _matrix = [
+  [4, 1, 2, 1, 0],
+  [6, 1, 1, 0, 1],
+  [10, 1, -1, -2, 3],
+];
+
+function zhardanoGaussMethod(matrix, k = 1, s = 4) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  console.log({ m, n });
+  let matrixCopy = JSON.parse(JSON.stringify(matrix));
+
+  drawMatrix(matrix);
+
+  console.log(`k: ${k}\ns: ${s}`);
+
+  if (matrix[k][s] <= 0) {
+    console.log("Элемент является не разрешимым");
+    return;
+  }
+
+  let ks = matrix[k][s];
+
+  for (let i = 0; i < matrix[k].length; i++) {
+    matrix[k][i] /= ks;
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i][s] /= -ks;
+  }
+
+  matrix[k][s] = -matrix[k][s];
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i != k && j != s) {
+        matrix[i][j] =
+          (matrixCopy[i][j] * matrixCopy[k][s] -
+            matrixCopy[i][s] * matrixCopy[k][j]) /
+          matrixCopy[k][s];
+      }
     }
+  }
 
-    let ks = matrix[k][s]
-    
-    for (let i = 0; i < matrix[k].length; i++) {
-        matrix[k][i] /= ks
-    }
-
-    for (let i = 0; i < matrix.length; i++) {
-        matrix[i][s] /= -ks
-    }
-
-    matrix[k][s] = -matrix[k][s]
-
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i != k && j != s) {
-                matrix[i][j] = (matrixCopy[i][j] * matrixCopy[k][s] - (matrixCopy[i][s] * (matrixCopy[k][j]))) / matrixCopy[k][s]
-            }
-        }
-    }
-    
-    drawMatrix(matrix)
-} 
-
-function drawMatrix(matrix) {
-    console.log( '\n\n')
-    for(let i = 0; i<matrix.length; i++) {
-        for(let j = 0; j<matrix[i].length; j++) {
-            matrix[i][j] = +matrix[i][j].toFixed(2)
-        }
-        console.log(matrix[i].join('  ') + '\n')
-    }
-}   
-    
-console.log(zhardanoGaussMethod(2, 3, 1, 2))
-
-let arr = [1, 3, 5, 7, 9, 10]
-///O(LogN)
-function yoptaBinarySearch(array, item) {
-    let low = 0
-    let high = array.length - 1
-
-    
-    while(low <= high) {
-        let mid = (low + high) / 2
-        if (mid % 2 !== 0 ) {
-            mid = Math.floor(mid)
-        }
-        let guess = array[mid]
-
-        if (guess == item) {
-            return mid
-        }
-
-        if (guess > item) {
-            high = mid -1
-        }
-        else {
-            low = mid + 1
-        }
-    }
- 
-    return "lox ebaniy net takogo elementa!"
+  drawMatrix(matrix);
+  return matrix;
 }
 
-console.log(yoptaBinarySearch(arr, 9))
+function drawMatrix(matrix) {
+  console.log("\n\n");
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrix[i][j] = +matrix[i][j].toFixed(2);
+    }
+    console.log(matrix[i].join("  ") + "\n");
+  }
+}
 
+const start = (matr = _matrix, k, s) => {
+  next(zhardanoGaussMethod(matr, k, s));
+};
+
+const next = (matr) =>
+  rl.question("resume? y/n:\n", function (bool) {
+    if (bool === "y") {
+      const ksnext = rl.question("enter k,s:\n", function (str) {
+        const [k, s] = str.split(",");
+        if (k === undefined || s === undefined) {
+          ksnext();
+        } else {
+          start(matr, Number(k), Number(s));
+        }
+      });
+    } else if (bool === "n") {
+      rl.close();
+    }
+  });
+
+rl.on("close", function () {
+  process.exit(0);
+});
+
+start();
